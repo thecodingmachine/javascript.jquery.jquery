435aad622cbab1bb6de26fcf35f74dd32d414d0f
David Négrier <d.negrier@thecodingmachine.com>
2018-08-28 09:34:51 +0200

    Removing Root_url

--- src/JQueryServiceProvider.php
+++ src/JQueryServiceProvider.php
@@ -4,7 +4,6 @@
 namespace Mouf\Javascript\JQuery;
 
 use Mouf\Html\Utils\WebLibraryManager\WebLibrary;
-use Psr\Container\ContainerInterface;
 use TheCodingMachine\Funky\Annotations\Factory;
 use TheCodingMachine\Funky\Annotations\Tag;
 use TheCodingMachine\Funky\ServiceProvider;
@@ -14,8 +13,8 @@ class JQueryServiceProvider extends ServiceProvider
     /**
      * @Factory(name="jQueryWebLibrary", tags={@Tag(name="webLibraries", priority=0.0)})
      */
-    public static function createRootUrlWebLibrary(ContainerInterface $container): WebLibrary
+    public static function createRootUrlWebLibrary(): WebLibrary
     {
-        return new WebLibrary(['vendor/mouf/javascript.jquery.jquery/jquery-1.11.0.min.js']);
+        return new WebLibrary(['vendor/mouf/javascript.jquery.jquery/jquery-1.11.0.min.js'], []);
     }
 }
